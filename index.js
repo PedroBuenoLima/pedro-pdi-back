8a933738014c08f1e85a324f4fd5c478063856af
Pedro <pedro.bueno@acct.global>
2023-06-26 09:34:29 -0300

    interligado back e front

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const repository = require('./repository')
+
+app.use(cors())
 
 app.use(express.json());
 
